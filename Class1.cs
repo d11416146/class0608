b0cb0c3bf7316d2a0ea08e50662958233df091cc
d11416146 <d11416146@gs.takming.edu.tw>
2026-06-08 14:36:49 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Student
+{
+	public int Mid;
+	public int Final;
+	public Student(int m,int f)
+	{
+		Mid = m;
+		Final = f;
+	}
+	public static Student operator+(Student a,Student b)
+	{
+		Student temp = new Student(0, 0);
+		temp.Mid = (a.Mid + b.Mid) / 2;
+		temp.Final = (a.Final + b.Final) / 2;
+		return temp;
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace class0608
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1=new Student (60,70);
+            Student s2 = new Student(75, 80);
+            Student savg = s1 + s2;
+            Console.WriteLine(savg.Mid + "" + savg.Final);
+        }
+    }
+}
